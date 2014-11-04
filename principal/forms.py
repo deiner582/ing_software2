4511cdc8de3742827c9b4009cda6466221275919
Libardo Cuello <libardoii@hotmail.com>
2014-11-04 08:58:54 -0500

    4/11/2014 ultima actualizacion

--- principal/forms.py
+++ principal/forms.py
@@ -3,13 +3,13 @@ from django import forms
 import datetime
 
 class FormRegistrarUsuario(forms.Form):
-    identificacion = forms.IntegerField(widget=forms.TextInput())
-    nombre = forms.CharField(widget=forms.TextInput())
-    apellido = forms.CharField(widget=forms.TextInput())
-    fecha_nacimiento = forms.DateField(widget=forms.DateInput(format='%d-%m-%Y'))
-    Username=forms.CharField(widget=forms.TextInput())
-    Password = forms.CharField(widget=forms.PasswordInput())
-    repeterir_Password=forms.CharField(widget=forms.PasswordInput(),label="Repetir Password")
+    identificacion = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control',}))
+    nombre = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',}))
+    apellido = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',}))
+    fecha_nacimiento = forms.DateField(widget=forms.DateInput(format='%d-%m-%Y', attrs={'class': 'form-control',}))
+    Username=forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control',}))
+    Password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control',}))
+    repeterir_Password=forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control',}),label="Repetir Password")
 
 class Formulario_iniciar_sesion(forms.Form):
     Username=forms.CharField(widget=forms.TextInput())
