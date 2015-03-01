1d5ef62b7d42a3dd617ecdf41a49c75ee54678a8
deiner582 <metallica082011@hotmail.com>
2015-03-01 07:35:23 -0500

    generar pdf

--- principal/views.py
+++ principal/views.py
@@ -1,19 +1,76 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import render_to_response, get_object_or_404
-from django.template import RequestContext
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponseRedirect
-import datetime
-from transporte.models import *
-from forms import *
+def vista_index(request):
+    return render_to_response('index.html',context_instance=RequestContext(request))
 
-# Create your views here.
+def vista_buses(request):
+    buses=Autobus.objects.all()
+    return render_to_response('buses.html',locals(),context_instance=RequestContext(request))
 
-def index(request):
-    return render_to_response('base.html',locals(),context_instance=RequestContext(request))
+def vista_conductores(request):
+    cond=Conductor.objects.all()
+    return  render_to_response('conductores.html',locals(),context_instance=RequestContext(request))
 
-def registrar(request):
+def vista_conductor_id(request, id_cond):
+    cond = Conductor.objects.get(identificacion=id_cond)
+    registro_entrada_salida = HoraEntradaSalida.objects.filter(conductor=id_cond)
+    historial = HistorialConductor.objects.filter(conductor=id_cond)
+    return render_to_response('condutor_detalle.html', locals(), context_instance=RequestContext(request))
+
+def vista_bus_placa(request, p):
+    bus = Autobus.objects.get(placa=p)
+    fallosMecanicos = ControlMecanico.objects.filter(autobus_placa=p)
+    revisiones = RevisionBus.objects.filter(autobus_placa=p)
+    return render_to_response('bus_detalle.html', locals(), context_instance=RequestContext(request))
+
+
+def comprar_viaje(request, cat):
+    viaje=Viaje.objects.filter(categoria=cat)
+    return render_to_response('comprar_viaje.html',locals(),context_instance=RequestContext(request))
+
+def imprimir_viaje(request, cod):
+    viaje=Viaje.objects.get(codigo=cod)
+    return render_to_response('imprimir_viaje.html',locals(),context_instance=RequestContext(request))
+
+def pdf(request, cod):
+    viaje=Viaje.objects.get(codigo=cod)
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename=""'+viaje.codigo+".pdf"
+
+    # Create the PDF object, using the response object as its "file."
+    p = canvas.Canvas(response,portrait(pagesize=A4))
+
+
+    p.drawImage('estaticos/img/billete.jpg',0,400,width=650,height=300)
+    p.drawImage('estaticos/img/transindia.jpg',250,700,width=128,height=128)
+    p.setFont('Times-Roman',16)
+    # Draw things on the PDF. Here's where the PDF generation happens.
+    # See the ReportLab documentation for the full list of functionality.
+    p.drawCentredString(200,602,viaje.codigo)
+    if viaje.tipo=="Ida":
+        p.drawCentredString(340,580,"Ida")
+        p.drawCentredString(250,570,"$ "+ str(viaje.precio_base_ida))
+        p.drawCentredString(280,530,"$ "+ str(viaje.categoria.costo))
+        p.drawCentredString(280,505,"$ "+ str(viaje.descuento))
+        p.drawCentredString(280,485,"$ "+ str(viaje.precio_total_ida))
+    else:
+        p.drawCentredString(340,580,"Id y Vu.")
+        p.drawCentredString(250,560,"$ "+ str(viaje.precio_base_ida_vuelta))
+        p.drawCentredString(280,530,"$ "+ str(viaje.categoria.costo))
+        p.drawCentredString(280,505,"$ "+ str(viaje.descuento))
+        p.drawCentredString(280,485,"$ "+ str(viaje.precio_total_ida_vuelta))
+
+    p.setFont('Times-Roman',12)
+    p.drawCentredString(485,517,str(viaje.origen))
+    p.drawCentredString(485,495,str(viaje.destino))
+    p.drawCentredString(500,475,str(viaje.categoria.nombre))
+
+
+
+    # Close the PDF object cleanly, and we're done.
+    p.showPage()
+    p.save()
+    return response
+
+def vista_registro(request):
     if request.method == 'POST':
         form = FormularioRegistro(request.POST)
         if form.is_valid():
@@ -27,13 +84,16 @@ def registrar(request):
             user.first_name = first_name
             user.last_name = last_name
             user.save()
-            return HttpResponseRedirect(reverse('index'))
+            return HttpResponseRedirect("/")
     else:
         form = FormularioRegistro()
-    data = {'form': form,}
-    return render_to_response('registrarse.html', data, context_instance=RequestContext(request))
+    return render_to_response('registro.html',locals(), context_instance=RequestContext(request))
 
-def loguear(request):
+def vista_billetes(request):
+    categoria=Categoria.objects.all()
+    return render_to_response('billetes.html',locals(),context_instance=RequestContext(request))
+
+def vista_iniciar_sesion(request):
     if request.method == 'POST':
         form = FormularioLogin(request.POST)
         if form.is_valid():
@@ -55,44 +115,16 @@ def loguear(request):
         form = FormularioLogin()
     return render_to_response('login.html', locals(), context_instance=RequestContext(request))
 
-def logout_view(request):
+def vista_logout(request):
     logout(request)
-    return HttpResponseRedirect('/')
+    return HttpResponseRedirect("/")
 
-def vista_buses(request):
-    buses=Autobus.objects.all()
-    return render_to_response('buses.html',locals())
+def serializado(request):
+    conductor = serializers.serialize("json",Categoria.objects.all())
+    return HttpResponse(conductor,content_type="application/json")
 
-def vista_conductores(request):
-    cond=Conductor.objects.all()
-    return  render_to_response('conductores.html',locals())
+def MainHomePage(request):
+        return render_to_response('index.html',context_instance=RequestContext(request))
 
-def vista_conductor_id(request,id_cond):
-    cond=Conductor.objects.get(identificacion=id_cond)
-    registro_entrada_salida=HoraEntradaSalida.objects.filter(conductor=id_cond)
-    historial=HistorialConductor.objects.filter(conductor=id_cond)
 
-    def _sueldo_total():
-        sueldo_total = 0
-        sum_horas_extras = 0
-        for rea in registro_entrada_salida:
-            sum_horas_extras = sum_horas_extras + rea.horas_extras
-        sueldo_total = cond.sueldo + sum_horas_extras * 25000
-        return sueldo_total
-        
-    sueldo_total = _sueldo_total
-    return render_to_response('condutor_detalle.html',locals(),context_instance=RequestContext(request))
 
-def vista_bus_placa(request,p):
-    bus=Autobus.objects.get(placa=p)
-    fallosMecanicos = ControlMecanico.objects.filter(autobus_placa=p)
-    revisiones = RevisionBus.objects.filter(autobus_placa=p)
-    return render_to_response('bus_detalle.html',locals(),context_instance=RequestContext(request))
-
-def comprar_viaje(request, cat):
-    viaje=Viaje.objects.filter(categoria=cat)
-    return render_to_response('comprar_viaje.html',locals(),context_instance=RequestContext(request))
-
-def vista_billetes(request):
-    categoria=Categoria.objects.all()
-    return render_to_response('billetes.html',locals())
